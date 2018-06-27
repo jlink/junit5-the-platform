6f77904dfbe604f9a7311be085195047e621608e
Johannes Link <business@johanneslink.net>
2018-06-27 12:10:45 +0200

    Added example with different engines in one class

---
+++ mixed-engines/src/test/java/mixed/MixedTests.java
@@ -0,0 +1,17 @@
+package mixed;
+
+public class MixedTests {
+
+//	@org.junit.jupiter.api.Test
+//	void junit5Test() {
+//	}
+//
+//	@org.junit.Test
+//	public void junit4Test() {
+//	}
+//
+//	@net.jqwik.api.Example
+//	void jqwikTest() {
+//	}
+
+}
